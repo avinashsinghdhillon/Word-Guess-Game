98db95ecf907edcddb08c73423e2b9234566ec5e
Avinash Singh <avinash.dhillon@gmail.com>
2019-05-30 12:47:12 -0500

    Added more JS code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,22 +4,44 @@ var dictionary = ["Unforgiven", "Tombstone", "Shane", "Stagecoach", "Winchester"
         "Bandidas", "Shadowheart", "Redemption", "Windwalker", "Silverado", "Django",
         "Independence", "Gunfighters", "Chisum", "Catlow", "Tecumseh"];
 
+var lettersUsed = new Array();
+
+//function to 
+function getQuizWord (){
+
+        return (dictionary[Math.floor(Math.random()* dictionary.length)]);
+}
+
+//function to create blanks for letters on screen
+
+
+//function to check if letter pressed exists in quiz word or lettersUsed array
+
+      
+      
+      
+function keyPressed (keyInput) {
+        if(lettersUsed.indexOf(keyInput) >= 0){
+                //letter has already been used
+                document.getElementById("Msg-Text").textContent = "Letter already guessed";
+        }else{
+                lettersUsed.push(keyInput);
+                document.getElementById("guessedLetters-Text").textContent = lettersUsed[0];
+        }
+}
+
 
 // this is the object: game 
 let game = {
     numGuessesRem: 0,
     lettersUsed: [],
-    //function to get quiz word from dictionary
-    quizWord: ()=> (dictionary[Math.floor(Math.random()* dictionary.length)]),
-
-    //function to create blanks for letters on screen
-    
-
-
-    //function to check if letter pressed exists in quiz word or lettersUsed array
-    // var tempWord = 
-
-    // document.getElementById("quizWord-Text").innerText = tempWord;
-    // return tempWord;
+    quizWord: dictionary[Math.floor(Math.random()* dictionary.length)],//get quiz word from dictionary
 };
 
+console.log(game.quizWord);
+document.getElementById("quizWord-Text").textContent = game.quizWord;
+
+document.onkeyup = function(event) {
+        var key = event.key;
+        keyPressed(key);
+}
